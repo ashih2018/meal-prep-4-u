341cd9c17138c4aae8c07694e3f8ccb88655bac5
Stella Cai <stella.sijiacai@gmail.com>
2019-11-17 01:52:00 -0500

    fdsjksjfhsd

--- project/pantry_vision.py
+++ project/pantry_vision.py
@@ -10,6 +10,7 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file("C:\\Users\\Stella\\Documents\\BostonHacks-56a57960ca1e.json")
 
 client = vision.ImageAnnotatorClient(credentials=credentials)
+# client = vision.ImageAnnotatorClient()
 # Instantiates a client
 
 # The name of the image file to annotate
@@ -24,6 +25,5 @@ image = vision.types.Image(content=content)
 objects = client.object_localization(
     image=image).localized_object_annotations
 
-print('Number of objects found: {}'.format(len(objects)))
 for object_ in objects:
     print(object_.name)
