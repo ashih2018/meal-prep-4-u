99b58a2e5261b7205e0a65baa56f3c2fb6d573b5
Aaron Pradhan <12a.pradhan@gmail.com>
2019-11-17 02:27:50 -0500

    Capabilities for saving past queries

--- project/http_requests.py
+++ project/http_requests.py
@@ -3,18 +3,52 @@ import json
 
 FIND_BY_INGREDIENTS = "https://api.spoonacular.com/recipes/findByIngredients"
 RECIPES_BULK = "https://api.spoonacular.com/recipes/informationBulk"
-RECIPES_FILE = "recipes.json"
-FULL_RECIPES_FILE = "full_recipes.json"
-
-API_KEY = ""
+QUERIES_FILE = "queries.json"
+API_KEY = "91c872bcb84c442f8599092ea7b1affb"
 
 
 def get_request(url, params, api_key):
+    """
+    A query is:
+    {"arguments":{"url":String, "params":Object},
+     "result":String}
+    The queries file contains a list of queries.
+    """
+    past_queries_raw = read_file(QUERIES_FILE)
+    past_queries = []
+    if past_queries_raw:
+        past_queries = json.loads(past_queries_raw)
+
+        for query in past_queries:
+            print(query["arguments"]["url"], query["arguments"]["params"].items())
+            print(url, params.items())
+            print(query["arguments"]["url"] == url and query["arguments"]["params"].items() <= params.items())
+
+        matching_queries = list(filter(
+            lambda args_dict:
+                args_dict["arguments"]["url"] == url and args_dict["arguments"]["params"].items() <= params.items(),
+            past_queries))
+        print(matching_queries)
+        if matching_queries:
+            print("There was a matching query: " + str(matching_queries[0]))
+            return matching_queries[0]["result"]
+        raise Exception("help")
+
+    print("Sending server request...")
     params["apiKey"] = api_key
     response = requests.get(url=url, params=params)
     if response.status_code == 400 or response.status_code == 401:
         raise Exception("Received server error")
-    return response.text
+    params.pop("apiKey")
+    query = {"arguments": {"url": url, "params": params}, "result": json.loads(response.text)}
+    past_queries.append(query)
+    write_file(QUERIES_FILE, json.dumps(past_queries))
+
+    return json.loads(response.text)
+
+
+def matches_args(url, params):
+    return lambda args_dict: args_dict["url"] == url and args_dict["params"] == params
 
 
 def read_file(filename):
@@ -30,39 +64,27 @@ def write_file(filename, text):
     f.close()
 
 
-def run():
-    recipes_json = read_file(RECIPES_FILE)
+def run(api_key):
+    # recipes_json = read_file(RECIPES_FILE)
+    #
+    # if not recipes_json:
 
-    if not recipes_json:
-        print("Sending server request...")
-        parameters = {"ingredients": "onions,tomatoes,garlic,parsley,beef",
-                      "number": 10,
-                      "ranking": 2}
+    parameters = {"ingredients": "onions,tomatoes,garlic,parsley,beef",
+                  "number": 10,
+                  "ranking": 2}
 
-        recipes_json = get_request(FIND_BY_INGREDIENTS, parameters, API_KEY)
-        write_file(RECIPES_FILE, recipes_json)
-
-    print(recipes_json)
-    recipes_array = json.loads(recipes_json)
+    recipes_array = get_request(FIND_BY_INGREDIENTS, parameters, api_key)
     print(recipes_array)
     recipe_ids_strings = list(map(lambda recipe: str(recipe["id"]), recipes_array))
     print(recipe_ids_strings)
     recipe_ids_string = ",".join(recipe_ids_strings)
     print(recipe_ids_string)
 
-    full_recipes_json = read_file(FULL_RECIPES_FILE)
-
-    if not full_recipes_json:
-        print("Sending server request...")
-        parameters = {"ids": recipe_ids_string,
-                      "includeNutrition": "true"}
-        full_recipes_json = get_request(RECIPES_BULK, parameters, API_KEY)
-        write_file(FULL_RECIPES_FILE, full_recipes_json)
-
-    print(full_recipes_json)
-    full_recipes_list = json.loads(full_recipes_json)
+    parameters = {"ids": recipe_ids_string,
+                  "includeNutrition": "true"}
+    full_recipes_list = get_request(RECIPES_BULK, parameters, api_key)
     print(full_recipes_list)
 
 
 if __name__ == '__main__':
-    run()
+    run(API_KEY)
