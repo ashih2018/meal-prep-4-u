6a2e2ddd1adb63c224df2b81b909fa2b7c63c444
Stella Cai <stella.sijiacai@gmail.com>
2019-11-17 06:19:15 -0500

    pantry pdate

--- project/pantry.py
+++ project/pantry.py
@@ -1,24 +1,37 @@
 import json
 import os
+import requests
+
+API_KEY = "89cc44a62bab4f498f7afb7257f799dc"
+BASE_URL = "https://api.spoonacular.com/recipes/parseIngredients?apiKey=" + API_KEY
 
 
-def read_category(category, name):
-    category = category.lower()
+def read_category(name):
     name = name.lower()
     if os.path.exists('pantry.json'):
         try:
             with open('pantry.json') as f:
-                data = json.load(f)
+                food = json.load(f)
         except ValueError:
-            data = {}
+            food = {}
     else:
-        data = {}
+        food = {}
 
-    if category in data:
-        if name not in data[category]:
-            data[category].append(name)
+    data = {'ingredientList': "broccoli",
+            'servings': 0,
+            'includeNutrition': False}
+    r = requests.post(url=BASE_URL, data=data)
+
+    if "aisle" not in r.json()[0]:
+        return
+
+    category = r.json()[0]["aisle"].lower()
+
+    if category in food:
+        if name not in food[category]:
+            food[category].append(name)
     else:
-        data[category] = [name]
+        food[category] = [name]
 
     with open('pantry.json', 'w') as outfile:
-        json.dump(data, outfile)
+        json.dump(food, outfile)
